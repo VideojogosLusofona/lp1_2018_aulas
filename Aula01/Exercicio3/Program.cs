d6b6f3c34146b6ed9fd70f20c9aa48ce56e14131
Nuno Fachada <nfachada@laseeb.org>
2019-03-14 15:43:36 +0000

    Adicionar ficheiros para a aula 1

---
+++ Aula01/Exercicio3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Aula01
+{
+    /// <summary>
+    /// This class contains the Main method.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// This is the Main() method. The program starts here.
+        /// </summary>
+        /// <param name="args">Command line arguments.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bye World!");
+            Console.Read();
+        }
+    }
+}
---
+++ Aula01/OlaMundo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Aula01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bye World!");
+            Console.Read();
+        }
+    }
+}
