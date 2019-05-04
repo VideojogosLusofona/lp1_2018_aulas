fa3a995d720c3d94889b1222431c61b0ec6289f7
Nuno Fachada <nuno.fachada@ulusofona.pt>
2019-05-04 19:27:01 +0100

    Adicionar exercício ColorsAndBallsWithProperties da aula 8

---
+++ Aula08/ColorsAndBallsWithProperties/Color.cs
@@ -0,0 +1,20 @@
+﻿namespace Aula07
+{
+    class Color
+    {
+        // Auto-implemented properties
+        public byte Red { get; set; }
+        public byte Green { get; set; }
+        public byte Blue { get; set; }
+        public byte Alpha { get; set; } = 255;
+
+        // Greyscale property - does not need support variable
+        public byte Greyscale
+        {
+            get
+            {
+                return (byte)((Red + Green + Blue) / 3);
+            }
+        }
+    }
+}
